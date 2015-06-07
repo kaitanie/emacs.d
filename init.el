fc77fed25f070fd00e3ddc7345ffaa79b1ba41d2
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2015-06-07 04:35:39 +0300

    Imported some code to install packages automatically from ELPA
    The installation example is from:
    http://stackoverflow.com/a/10095853

--- init.el
+++ init.el
@@ -5,7 +5,44 @@
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize) ;; You might already have this line
+
+(defun ensure-package-installed (&rest packages)
+  "Assure every package is installed, ask for installation if it’s not.
+
+Return a list of installed packages or nil for every skipped package."
+  (mapcar
+   (lambda (package)
+     ;; (package-installed-p 'evil)
+     (if (package-installed-p package)
+         nil
+       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+           (package-install package)
+         package)))
+   packages))
+
+;; make sure to have downloaded archive description.
+;; Or use package-archive-contents as suggested by Nicolas Dudebout
+(or (file-exists-p package-user-dir)
+    (package-refresh-contents))
+
+(ensure-package-installed 'magit
+                          'cider
+                          'haskell-mode
+                          'scala-mode2
+                          'haskell-snippets
+                          'ghc
+                          'org
+                          'clojure-snippets
+                          'lusty-explorer
+                          'paredit
+                          'company
+                          'company-cabal
+                          'company-ghc
+                          'company-ghci
+                          'rainbow-mode) ;  --> (nil nil) if iedit and magit are already installed
+
+;; activate installed packages
+(package-initialize)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
