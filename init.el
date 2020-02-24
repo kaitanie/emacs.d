d892ed0cf926e75dd8de3bdacec059ef8f9c34a7
Pekka Kaitaniemi <pekka.kaitaniemi@tocoman.com>
2020-02-24 17:12:00 +0200

    Office computer modifications

--- init.el
+++ init.el
@@ -98,6 +98,7 @@
                                   (company-mode)
                                   (flycheck-mode)
                                   (global-set-key (kbd "<f11>") 'psc-ide-goto-definition)
+                                  (global-set-key (kbd "<f10>") 'psc-ide-load-module)
                                   ;;(turn-on-purescript-indentation)
                                   ))
 
@@ -369,7 +370,14 @@ Then move to that line and indent accordning to mode"
 ;; Haskell
 
 (use-package lsp-mode
-  :ensure t)
+  :ensure t
+    ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  :init (setq lsp-keymap-prefix "M-l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         ;; (XXX-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
 
 (use-package company-lsp
   :ensure t
@@ -521,6 +529,12 @@ Then move to that line and indent accordning to mode"
 
 (use-package lsp-mode
   :ensure t
+    ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  :init (setq lsp-keymap-prefix "M-l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         ;; (XXX-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp
   :config (require 'lsp-clients))
 
