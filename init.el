d25ed847ec4ea8eeeb13b3a9ceb7a0fe74f735fb
Pekka Kaitaniemi <pekka.kaitaniemi@tocoman.com>
2021-11-03 19:39:59 +0200

    Change org indentation level

--- init.el
+++ init.el
@@ -70,6 +70,7 @@
  '(global-flycheck-mode t)
  '(inhibit-startup-screen t)
  '(merlin-completion-with-doc t)
+ '(org-indent-indentation-per-level 0)
  '(package-selected-packages
    '(mu4e lsp-ivy rustic rustic-mode haskell-mode ag magit s dash-functional flycheck-rust cargo toml-mode lsp-ui javascript-eslint web-mode wand lsp-haskell lsp-mode company-lsp rust-mode highlight-indentation highlight-indent-guides-mode markdown-mode nix-mode counsel swiper ivy use-package csv-mode overcast-theme flycheck evil-lisp-state evil-collection projectile evil abyss-theme xkcd utop undo-tree typed-clojure-mode systemd sos react-snippets rainbow-mode opam magit-gitflow lusty-explorer jsx-mode haskell-snippets hackernews gist flx-ido company-ghc company-cabal clojure-snippets clj-refactor))
  '(rustic-lsp-client 'eglot)
